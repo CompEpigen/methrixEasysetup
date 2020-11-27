0ec6deb1b514fc5ea7dbdac205b3061bf0febf4d
Sivanesan Rajan <65311009+rajansiv@users.noreply.github.com>
2020-11-27 12:22:25 +0100

    Add files via upload

---
+++ server.R
@@ -0,0 +1,189 @@
+
+
+server <-   function(input, output, session){
+    shinyjs::useShinyjs()
+    volumes = getVolumes()
+    observe({
+      shinyFileChoose(input, "btn", roots = volumes, session = session)
+      if(!is.null(input$btn)){
+        slctd_file<-parseFilePaths(volumes, input$btn)
+        direct<- renderText(as.character(slctd_file$datapath[1]))
+      }
+      observeEvent(input$pre,{
+        output$previewtable <- renderTable({
+          req(input$btn)
+          fread(direct(), header = input$header, sep = "\t", nrows = 5)
+        })
+        output$previewfilename <- renderText({
+          paste("Preview of the file: ",  as.character((basename(slctd_file$datapath[1]))))
+        })
+      })
+    })
+    
+    volumes = getVolumes()
+    observe({ 
+      input$btn
+      if(!is.null(input$btn)){
+        slctd_file<-parseFilePaths(volumes, input$btn)
+        # output$filename<- renderText(as.character(tools::file_path_sans_ext(basename(slctd_file$datapath))))
+        filenames <- reactive({
+          data.frame(
+            as.vector(
+              sub(
+                pattern= "(.*?)\\..*$",
+                replacement = "\\1",
+                basename(slctd_file$datapath)
+              )
+            )
+          )
+        })
+        saraw<- reactive({
+          req(input$sampleanno)
+          insampleanno <- input$sampleanno
+          read.csv(insampleanno$datapath, sep = ",", header = TRUE)
+        })
+        
+        filenames <- reactive({
+          req(input$btn)
+          data.frame(
+            as.character(
+              basename(slctd_file$datapath)
+            )
+          )
+        })
+        
+        output$sampleannoTable <- renderTable({
+            req(input$sampleanno)
+            cbind(filenames(), saraw())
+        })
+      }
+    }
+    )
+    
+    observeEvent(input$tab1Next,{
+      updateTabsetPanel(session, 
+                        "mES1",
+                        selected = "readIn")
+    })
+    
+    dataCode <- codeGeneration("dataCode",
+                               readInParametersInput,
+                               projectDetailsUI,
+                               stringsAsFactors = TRUE)
+    
+    observeEvent(input$code,{
+      output$generatedCode <- renderText ({
+        dataCode()
+      })})
+    observeEvent(input$startWorkflowR,{
+    sink("analysis/read_in.Rmd")  
+    dataCode()
+    sink()
+    return(read_in.Rmd)
+    })
+    
+    
+    
+    observeEvent(input$tab2Previous,{
+      updateTabsetPanel(session,
+                        "mES1",
+                        selected = "start")
+    })
+    
+    observeEvent(input$tab2Next,{
+      updateTabsetPanel(session,
+                        "mES1",
+                        selected = "preprocess")
+    })
+    
+    output$intialQC <- renderPrint ({
+      
+      cat("methrix::methrix_report(\n
+          meth = meth,\n
+          output_dir = getwd(), \n
+          prefix = \"initial\")")
+    })
+    
+    output$coverageBasedFiltering <- renderPrint({
+      
+      cat("meth <- methrix::mask_methrix(\n
+          meth,\n
+          low_count = 2,\n
+          high_quantile = 0.99)")
+    })
+    
+    output$coverageRemoval <- renderPrint({
+      
+      cat("meth <- methrix::remove_uncovered(meth)\n
+          meth <- methrix::coverage_filter(\n
+          m=meth,\n
+          cov_thr = 5,\n
+          min_samples = 2)")
+    })
+    
+    output$snpFiltering <- renderPrint({
+      cat("meth <- methrix::remove_snps(m = meth, keep = TRUE)")
+    })
+    
+    output$methrixReport <- renderPrint({
+      cat(" methrix::methrix_report(meth = meth,\n
+          output_dir = getwd(),\n
+          recal_stats = TRUE,\n
+          prefix=\"processed\")")
+    })
+    
+    observeEvent(input$code2,{
+      zz <- file("analysis/methrix_report.Rmd", open = "wt")
+      sink(zz)
+      cat("---\n")
+      cat("title: ",input$projectName,"\n")
+      cat("author: ",input$authorName,"\n")
+      cat("date: \'\`r format(Sys.time(), \"%d %B, %Y\")\`\'\n")
+      cat("output: workflowr::wflow_html\n")
+      cat("editor_options:\n")
+      cat("\t chunk_output_type: console\n")
+      cat("---\n")
+      cat("\n")
+      cat("\`\`\`{r libraries, message=TRUE, warning=FALSE, include=FALSE}\n
+        # Installing libraries\n")
+      cat("library(methrix)\n")
+      cat("library(data.table)\n")
+      cat("library(shinyFiles)\n")
+      cat("library(shinyDashboard)\n")
+      cat("library(dmrseq)\n")
+      cat("library(DSS)\n")
+      cat("library(rGREAT)\n")
+      cat("library(rtracklayer)\n")
+      cat("library(Gviz)\n")
+      cat("library(biomaRt)\n")
+      cat("library(pheatmap)\n")
+      cat("\n")
+      cat("\`\`\`\n")
+      cat("\n")
+      cat("\`\`\`{r libraries, message=TRUE, warning=FALSE, include=FALSE}\n")
+      cat("\n")
+      cat(" # Intial QC and summary \n
+      methrix::methrix_report(\n
+          meth = meth,\n
+          output_dir = getwd(), \n
+          prefix = \"initial\")\n")
+      cat("\`\`\`\n
+\`\`\`{r libraries, message=TRUE, warning=FALSE, include=FALSE}\n")
+      cat(" # Coverage based filtering\n")
+      cat("meth <- methrix::mask_methrix(\n
+          meth,\n
+          low_count = 2,\n
+          high_quantile = 0.99)\n")
+      cat("meth <- methrix::remove_uncovered(meth)\n
+          meth <- methrix::coverage_filter(\n
+          m=meth,\n
+          cov_thr = 5,\n
+          min_samples = 2)\n")
+      cat("meth <- methrix::remove_snps(m = meth, keep = TRUE)")
+      cat("\`\`\`\n")
+      close(zz)
+    })
+    
+}
+
+
---
+++ ui.R
@@ -0,0 +1,126 @@
+
+
+ui <- dashboardPage(
+  dashboardHeader(
+    title = "methrix Easy Setup"
+  ),
+  dashboardSidebar(
+    disable = TRUE
+  ),
+  dashboardBody(
+    navbarPage(
+      "",
+      id = "mES1",
+      tabPanel(
+        "Start",
+        value = "start",
+        fluidRow( 
+          column(
+            width = 4,
+            box(
+              projectDetailsUI()
+              )
+          )
+        )
+      ),
+      tabPanel(
+        "Reading in the bedGraph files",
+        value = "readIn",
+        fluidRow(
+          column(
+            width = 4,
+            box(
+              title = "Select the file locally",
+              width = NULL,
+              fileIn())
+          ),
+          column(
+            width = 8,
+            box(
+              title = "Preview of the first bedGraph file",
+              solidHeader = TRUE,
+              width = NULL,
+              status = "warning",
+              verbatimTextOutput("previewfilename"),
+              tableOutput("previewtable")
+              ),
+           
+              readInParametersInput(),
+            box(
+              title = "Sample Annotation",
+              solidHeader = TRUE,
+              width = NULL,
+              status = "warning",
+              sampleAnnotationUI()
+            ),
+            box(
+              title = "Generated Code",
+              solidHeader = TRUE,
+              width = NULL,
+              status = "warning",
+              verbatimTextOutput("generatedCode"),
+              actionButton(
+                inputId = "tab2Previous",
+                label = "Previous",
+                icon = icon("arrow-left")
+              ),
+              actionButton(
+                inputId = "startWorkflowR",
+                label = "Start WorkflowR"
+              ),
+              actionButton(
+                inputId = "tab2Next",
+                label = "Next",
+                icon = icon("arrow-right")
+              )
+            )
+          )
+        )
+      ),
+      
+      tabPanel(
+        "Preprocessing, Normalization and QC",
+        value = "preprocess",
+        fluidRow(
+          column(
+            width = 3,
+            box( width = NULL,
+              preprocessInput()
+            )
+            ),
+            column(
+              width =4 ,
+              box(
+                width = NULL,
+                title = "Intial QC",
+                verbatimTextOutput("intialQC")
+              ),
+              
+              box(
+                width = NULL,
+                title = "Visualization and QC after filtering",
+                h4("Methrix Report"),
+                verbatimTextOutput("methrixReport")
+              )
+              
+            ),
+          column (
+            width=4,
+            box(
+              width = NULL,
+              title = "Filtering",
+              h4("Coverage based filtering"),
+              verbatimTextOutput("coverageBasedFiltering"),
+              h5("Removing uncovered sites"),
+              verbatimTextOutput("coverageRemoval"),
+              h4("SNP filtering"),
+              verbatimTextOutput("snpFiltering")
+            )
+          )
+            
+          
+        )     
+      )
+    )
+  )
+)
